bea80bc9b3ca2815a7cd4377ac43065edeb5819f
gauravs1024 <gauravs1024@gmail.com>
2024-09-20 15:04:40 +0530

    first commit

---
+++ src/main/java/Controller_java/Login.java
@@ -0,0 +1,18 @@
+package Controller_java;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("loginForm")
+public class Login extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	} 
+
+}
